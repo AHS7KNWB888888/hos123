0f44eb9ed304ca80378f7b3598d2748957256f89
AHS7KNWB888888 <sajd.uoquw82@gmail.com>
2024-05-14 17:25:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/hossamhosss/hos123",
+    "https://github.com/AHS7KNWB888888/hos123",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
